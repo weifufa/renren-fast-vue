ce6fffd141f1b5f98162f58cd7c090397d0c6114
weifufa <1461531243@qq.com>
2022-10-17 12:58:52 +0800

    添加商品分类删除

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,33 +1,58 @@
 <template>
-  <el-tree
-    :data="menus"
-    :props="defaultProps"
-    :expand-on-click-node="false"
-    show-checkbox
-    node-key="catId"
-  >
-    <span class="custom-tree-node" slot-scope="{ node, data }">
-      <span>{{ node.label }}</span>
-      <span>
-        <el-button
-          v-if="node.level <= 2"
-          type="text"
-          size="mini"
-          @click="() => append(data)"
-        >
-          Append
-        </el-button>
-        <el-button
-          v-if="node.childNodes.length == 0"
-          type="text"
-          size="mini"
-          @click="() => remove(node, data)"
-        >
-          Delete
-        </el-button>
+  <div>
+    <el-tree
+      :data="menus"
+      :props="defaultProps"
+      :expand-on-click-node="false"
+      show-checkbox
+      node-key="catId"
+      :default-expanded-keys="expandedkey"
+    >
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            v-if="node.level <= 2"
+            type="text"
+            size="mini"
+            @click="() => append(data)"
+          >
+            Append
+          </el-button>
+           <el-button
+            v-if="node.level <= 2"
+            type="text"
+            size="mini"
+            @click="edit(data)"
+          >
+            Edit
+          </el-button>
+          <el-button
+            type="text"
+            size="mini"
+            @click="() => remove(node,data)"
+          >
+            Delete
+          </el-button>
+        </span>
+      </span></el-tree
+    >
+    <el-dialog
+      title="提示"
+      :visible.sync="dialogVisible"
+      width="30%"
+    >
+      <el-form :model="category">
+        <el-form-item label="活动名称">
+          <el-input v-model="category.name" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addCategory()">确 定</el-button>
       </span>
-    </span></el-tree
-  >
+    </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -39,7 +64,10 @@ export default {
   props: {},
   data() {
     return {
+      category: { name: "", parentCid: 0, catLevel: 0, showStatus: 1, sort: 0 },
+      dialogVisible: false,
       menus: [],
+      expandedkey: [],
       defaultProps: {
         children: "children",
         label: "name",
@@ -56,19 +84,59 @@ export default {
         this.menus = data.data;
       });
     },
+    edit(data) {
+console.log("要修改的数据",data)
+    },
     append(data) {
+      this.dialogVisible = true;
+    this.category.parentCid=data.catId;
+     this.category.catLevel=data.catLevel*1+1;//转换数字再+1
       console.log("append", data);
+    },
+    //添加三三级分类
+    addCategory() {
+      console.log("提交的三级分类数据", this.category);
+      this.$http({
+      url: this.$http.adornUrl('/product/category/save'),
+      method: 'post',
+      data: this.$http.adornData(this.category, false)
+      }).then(({ data }) => {
+         this.$message({
+              message: "菜单保存成功！",
+              type: "success",
+            });
+       });
+       //关闭对话框
+       this.dialogVisible=false;
+        //刷新出新的菜单
+        this.getMenus();
+         //设置需要默认展开的菜单
+         this.expandedkey = [this.category.parentCid];
     },
     remove(node, data) {
       var ids = [data.catId];
-      this.$http({
-        url: this.$http.adornUrl("/product/category/delete"),
-        method: "post",
-        data: this.$http.adornData(ids, false),
-      }).then(({ data }) => {
-        console.log("删除成功")
-        this.getMenus();
-      });
+      this.$confirm(`是否删除当前【${data.name}】菜单?`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
+          this.$http({
+            url: this.$http.adornUrl("/product/category/delete"),
+            method: "post",
+            data: this.$http.adornData(ids, false),
+          }).then(({ data }) => {
+            this.$message({
+              message: "菜单删除成功！",
+              type: "success",
+            });
+            //刷新出新的菜单
+            this.getMenus();
+            //设置需要默认展开的菜单
+            this.expandedkey = [node.parent.data.catId];
+          });
+        })
+        .catch(() => {});
       console.log("remove", node, data);
     },
   },
